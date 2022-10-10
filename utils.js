729b4f33fddf750184f7c6b51340391bec17e6d2
Liluminalia <peloazul4@gmail.com>
2022-10-10 16:23:50 +0200

    solve code smelling comments

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
+// existe otra forma de implementar las funciones
+// mediante declaracion
 // export function bar () {
 //     const x = 22 * 2;
-
 //     if (x > 12) {
 //         console.log(x);
 //     }
